066eff62b165af67899fcaa86511b983ee30b77d
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-06-06 16:02:08 -0700

    Updated html
    Added more functions to fetch APIs

--- scripts/app.js
+++ scripts/app.js
@@ -22,5 +22,30 @@ function DisplayQuote() {
         //Write a conditional that checks API, and outputs that its busy 
         myQuote.innerHTML = `"${quote.quote}"\n - ${quote.author}`
     })
+
+    DisplayMeme();
+    DisplayCatFact();
 }
 
+function DisplayMeme() {
+    const base_URL = "https://api.imgflip.com/get_memes";
+    const myImage = document.getElementById("myImage")
+
+    fetch(base_URL)
+    .then(response => response.json())
+    .then(memes => {
+        //Write a conditional that checks API, and outputs that its busy 
+        myImage.src = memes.data.memes[Math.floor(Math.random()*100)].url;
+    })
+}
+
+function DisplayCatFact() {
+    const catFact = document.getElementById("catFact")
+
+    fetch("https://meowfacts.herokuapp.com/")
+    .then(response => response.json())
+    .then(fact => {
+        //Write a conditional that checks API, and outputs that its busy 
+        catFact.innerHTML = "Cat Fact: " + fact.data[0];
+    })
+}
